34ea511483cd4c00e768ef0c328737efda3e98b0
Despina <you@example.com>
2022-05-22 00:42:26 +0300

    new database.js

--- databaseUI/utils/database.js
+++ databaseUI/utils/database.js
@@ -2,11 +2,20 @@ const mysql = require('mysql2');
 
 /* create connection and export it */
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB,
-});
+   // host: process.env.DB_HOST,
+    host: 'localhost',
+    //port: process.env.DB_PORT,
+    port: 3000,
+    user: 'root',
+    password: 'DespoinaTomkou2903!!',
+    database:'ourdatabase'
+    //user: process.env.DB_USER,
+   // password: process.env.DB_PASS,
+    //database: process.env.DB,
+})
+
+pool.query("select * from ourdatabase.Field", (err,res)=>{
+    return console.log(res)
+})
 
 module.exports = { pool };
