52b968a633365b53b924c0fb8dfb98a9a3ba67b3
Despina <you@example.com>
2022-05-28 10:34:25 +0300

    add person router and ejs

---
+++ databaseUI/routes/routes/person.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const personController = require('../../controllers/controllers/person');
+
+const router = express.Router();
+
+router.get('/', personController.getPerson);
+//router.get('/student-creation-page', layoutController.getCreateStudent)
+
+module.exports = router;
