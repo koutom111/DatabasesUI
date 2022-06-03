9ced8e7465a71c40fbdd8f24c967cef122284b67
fwtostatho <fotinist01@gmail.com>
2022-06-03 10:39:27 +0300

    3.1

--- databaseUI/controllers/controllers/project.js
+++ databaseUI/controllers/controllers/project.js
@@ -9,6 +9,9 @@ exports.getProject = (req, res, next) => {
 
     let ProjectTitle= [];
     let Summary = [];
+    let Date = [];
+    let Duration = [];
+    let Executive = [];
 
     /* create the connection */
     pool.getConnection((err, conn) => {
@@ -16,14 +19,21 @@ exports.getProject = (req, res, next) => {
         /* execute query to get best dribbler */
         let namePromise = new Promise((resolve, reject) => {
             conn.promise()
-                .query("select Project_Title, Summary from Project")
+                .query("select P.Project_Title AS Project_Title, P.Summary AS Summary, P.Starting_Date AS Starting_Date, P.Duration AS Duration,CONCAT(First_Name , ' ' , Last_Name) AS ExName from Project P inner join Executive E on P.Executive_ID=E.Executive_ID")
                 .then(([rows, fields]) => {      //??????
                     rows.forEach(element=>{
                         ProjectTitle.push(element.Project_Title);
                         Summary.push(element.Summary);
+                        Date.push(element.Starting_Date);
+                        Duration.push(element.Duration);
+                        Executive.push(element.ExName);
+
                     })
                     console.log(ProjectTitle);
                     console.log(Summary);
+                    console.log(Date);
+                    console.log(Duration);
+                    console.log(Executive);
                     resolve();
                 })
                 .then(() => pool.releaseConnection(conn))
@@ -37,6 +47,9 @@ exports.getProject = (req, res, next) => {
                 pageTitle: "See All our Projects! ",
                 ProjectTitle ,
                 Summary,
+                Date,
+                Duration,
+                Executive,
                 messages
             })
         });
