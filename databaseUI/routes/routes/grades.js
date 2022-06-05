614a49a78801073c79cb6e712f97ce3d60e26d38
Despina <you@example.com>
2022-06-05 23:17:51 +0300

    final!

--- databaseUI/routes/routes/grades.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const gradesController = require('../controllers/grades');
-
-const router = express.Router();
-
-router.get('/', gradesController.getGrades);
-router.post('/delete/:id', gradesController.postDeleteGrade);
-
-module.exports = router;
--- databaseUI/routes/routes/layout.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const researcherController = require('../../controllers/controllers/researcher');
-
-const router = express.Router();
-
-router.get('/', researcherController.getLanding);
-router.get('/student-creation-page', researcherController.getCreateStudent)
-
-module.exports = router;
